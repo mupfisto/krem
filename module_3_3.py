f186cf8c04035b991304593edc23fc166aba1123
mupfisto <newtonforce326@gmail.com>
2025-01-16 16:47:20 +0300

    module_3_3

---
+++ module_3_3.py
@@ -0,0 +1,36 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params()
+
+value_list = [1, 'питон', False]
+print_params(2, 'столбец', False)
+print_params(3, 'None')
+print_params(a='один', b='два', c='три')
+print_params(a='один', b='два')
+print_params(b='два', c='три')
+print_params(a='один', c='три')
+print_params(a='один')
+print_params(b='два')
+print_params(c='три')
+print_params()
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+
+values_list = [9.8, 'const', True]
+values_dict = {'a': 'else', 'b': False, 'c': 0}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+def append_to_list(item, values_list=None):
+    if values_list is None:
+        values_list = []
+        values_list.append(item)
+
+print_params(*values_list)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
